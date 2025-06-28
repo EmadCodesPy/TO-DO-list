09fde666de9d0e569fd809aec2f2e66506322694
Emadaddin <Emad@MacBook-Air.local>
2025-06-28 15:18:38 +0300

    Added a function to remove complete or incomplete tasks

--- to-do_list.py
+++ to-do_list.py
@@ -1,6 +1,6 @@
 #a to-do list
 #features include: General select, Adding a to-do, Checking the list, 
-#Finishing a to-do, Deleting a to-do, Making a new list
+#Finishing a to-do, Deleting a to-do
 import sqlite3
 
 conn = sqlite3.connect('/Users/Emad/Desktop/Github port/to-do/to-do-list.db')
@@ -47,6 +47,21 @@ def finish():
         finish_task(task)
         print ('✅ ' + task)
     conn.commit()
+
+#Removing a task
+def remove():
+    def remove_task(task):
+        c.execute("DELETE FROM 'do_list' WHERE finished=? OR finished=? AND task=?", ('❌','✅',task))
+        print('REMOVED TASK: ' + task)
+    removing = True
+    while removing:
+        task = input('What tasks would you like to remove? (Leave empty to stop removing): ')
+        if task == '':
+            removing = False
+            continue
+        remove_task(task)
+    conn.commit()
+
 #Select your function
 func = input('What would you like to do. Add a task(A), Check your tasks(C), Finish a task(F), Remove a task(R): ')
 if func.upper() == 'A':
@@ -55,4 +70,6 @@ elif func.upper() == 'C':
     checking()
 elif func.upper() == 'F':
     finish()
+elif func.upper() == 'R':
+    remove()
 conn.close()
