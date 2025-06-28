c92b4e7c1e2103ef1a08c1e6781b7ab28994b773
Emadaddin <Emad@MacBook-Air.local>
2025-06-28 14:37:15 +0300

    Allowed for selection of function

--- to-do_list.py
+++ to-do_list.py
@@ -1,6 +1,6 @@
 #a to-do list
-#features include: Adding a to-do, Checking the list, Finishing a to-do, Deleting a to-do, Making a new list
-# -*- coding: utf-8 -*-
+#features include: General select, Adding a to-do, Checking the list, 
+#Finishing a to-do, Deleting a to-do, Making a new list
 import sqlite3
 
 conn = sqlite3.connect('/Users/Emad/Desktop/Github port/to-do/to-do-list.db')
@@ -13,17 +13,23 @@ c = conn.cursor()
     #""")
 
 #Adding a task
-def add_task(task):
-    task = task
-    c.execute(f"INSERT INTO 'do_list' VALUES (?,?) ", ('❌', task))
-#loop for adding task multiple times
-adding = True
-while adding:
-    task = input('What task would you like to add? (Leave empty to stop adding)')
-    if task == '':
-        adding = False
-        continue
-    add_task(task)
+def adding():
+    def add_task(task):
+        task = task
+        c.execute(f"INSERT INTO 'do_list' VALUES (?,?) ", ('❌', task))
+    #loop for adding task multiple times
+    adding = True
+    while adding:
+        task = input('What task would you like to add? (Leave empty to stop adding)')
+        if task == '':
+            adding = False
+            continue
+        add_task(task)
+    conn.commit()
 
-conn.commit()
+#Select your function
+func = input('What would you like add a task(A), Check your tasks(C), Remove a task(R): ')
+if func.upper() == 'A':
+    adding()
+
 conn.close()
